885284e68cffd50d2ae0a3443cd765f93fb011ae
abiola-adeoye <slmadeoye@gmail.com>
2023-01-09 21:21:38 +0100

    update

--- app.py
+++ app.py
@@ -4,5 +4,13 @@ from flask import Flask
 from flask_restful import Api
 
 from db import db
+from dotenv import load_dotenv
 
 
+
+app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
